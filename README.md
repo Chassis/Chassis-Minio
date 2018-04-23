ae1847edc79057f6db6965c901691bc2cb503f86
Robert O'Rourke <robert@o-rourke.org>
2018-04-23 11:00:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Minio
+A Chassis extension to install and configure the Minio server and client on your Chassis server.
+
+[Minio](https://www.minio.io/) is an open source self-hosted alternative to Amazon S3 with a compatible API.
+
+## Usage
+1. Add this extension to your extensions directory `git clone git@github.com:Chassis/Chassis-Minio.git extensions/chassis-minio`
+2. Run `vagrant provision`.
+3. Copy the `minio-mu-plugin.php` file to your `mu-plugins` directory to get S3 Uploads working.
+
+Your existing uploads will be synced to the Minio server automatically.
+
+After provisioning you can browse to http://vagrant.local:4571/ to view the web interface and explore your bucket contents.
+
+## Configuration options
+You can configure the port used by Minio server in your chassis config file.
+```yaml
+minio:
+  port: 1234
+```
+
+Depending on how you connect to S3 you may need to set the S3 server path and region.
+
+Check the `local-config.php` for the settings you can define.
+
+Constants are already configured to work with the [S3 Uploads plugin](https://github.com/humanmade/S3-Uploads) so if you use that then there's nothing further to do!
---
+++ modules/chassis-minio/manifests/init.pp
@@ -0,0 +1,111 @@
+# A Chassis extension to install and configure Fake S3
+class chassis-minio (
+  $config,
+) {
+
+  # Default settings for install
+  $defaults = {
+    'port' => 4571,
+    'sync' => true,
+  }
+
+  # Allow override from config.yaml
+  $options = deep_merge($defaults, $config[fakes3])
+
+  if ( !empty($config[disabled_extensions]) and 'chassis/chassis-minio' in $config[disabled_extensions] ) {
+
+    class { 'minio':
+      package_ensure => 'absent'
+    }
+
+    file { '/home/vagrant/mc':
+      ensure => 'absent',
+    }
+
+    file { '/usr/local/bin/mc':
+      ensure => 'absent',
+    }
+
+    file { '/home/vagrant/.mc':
+      ensure => 'absent',
+    }
+
+    file { '/vagrant/extensions/chassis-minio/local-config.php':
+      ensure => 'absent'
+    }
+
+  } else {
+
+    $port = $options[port]
+
+    class { 'minio':
+      package_ensure => 'present',
+      listen_ip      => '127.0.0.1',
+      listen_port    => $port,
+      configuration  => {
+        'credential' => {
+          'accessKey' => 'ADMIN',
+          'secretKey' => 'PASSWORD',
+        },
+        'region'     => 'us-east-1',
+        'browser'    => 'on',
+      }
+    }
+
+    # Install Minio Client
+    exec { 'mc':
+      command => '/usr/bin/wget https://dl.minio.io/client/mc/release/linux-amd64/mc',
+      unless  => '/usr/bin/test -f mc',
+      cwd     => '/home/vagrant',
+      require => [ Service['minio'] ],
+    } ->
+    exec { '/bin/chmod +x mc':
+      cwd    => '/home/vagrant',
+      unless => '/usr/bin/test `which mc`'
+    } ->
+    exec { '/bin/ln -sf /home/vagrant/mc /usr/local/bin/mc':
+      unless => '/usr/bin/test `which mc`',
+    } ->
+    file { '/home/vagrant/.mc':
+      ensure => 'directory',
+      owner  => 'vagrant',
+    } ->
+    file { '/home/vagrant/.mc/config.json':
+      ensure  => 'present',
+      content => template('chassis-minio/config.json.erb'),
+      owner   => 'vagrant',
+    }
+
+    # Create default bucket.
+    exec { 'mc mb local/chassis':
+      command => "/usr/local/bin/mc mb local/chassis",
+      user    => 'vagrant',
+      unless  => "/usr/local/bin/mc ls local/chassis",
+      require => Exec['mc'],
+    } ->
+    exec { 'mc policy public local/chassis':
+      command => '/usr/local/bin/mc policy public local/chassis',
+      unless  => '/usr/local/bin/mc policy local/chassis | grep "public"',
+    }
+
+    $content = $config[mapped_paths][content]
+
+    # Sync existing uploads
+    exec { "mc mirror ${content}/uploads local/chassis/uploads":
+      command => "/usr/local/bin/mc mirror ${content}/uploads local/chassis/uploads",
+      user    => 'vagrant',
+      onlyif  => "/usr/bin/test -d ${content}/uploads",
+      require => [
+        Exec['mc mb local/chassis'],
+      ],
+    }
+
+    # Configure WP
+    file { '/vagrant/extensions/chassis-minio/local-config.php':
+      ensure  => 'present',
+      content => template('chassis-minio/local-config.php.erb'),
+    }
+
+  }
+
+}
